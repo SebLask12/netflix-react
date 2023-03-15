33466189792517a4c0e7708b35303448ff56228b
SebLask12 <sebastian.laskowski15@gmail.com>
2023-03-15 18:48:30 +0100

    add slider

--- src/modules/HomeModule/pages/HomePage/Categories/Slider.tsx
+++ src/modules/HomeModule/pages/HomePage/Categories/Slider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import MovieItem from "@/global-components/MovieItem";
 
@@ -10,7 +10,8 @@ import Image3 from "../../../../../assets/images/section1/3.png";
 import Image4 from "../../../../../assets/images/section1/4.png";
 import Image5 from "../../../../../assets/images/section1/5.png";
 import Image6 from "../../../../../assets/images/section1/6.png";
-import { type } from "os";
+
+import ArrowRight from "../../../../../assets/images/arrow-down.svg";
 
 type Image = {
   id: number;
@@ -32,18 +33,93 @@ const images: Image[] = [
 ];
 
 type Props = {
-    title: string,
-}
+  title: string;
+};
 
 const Slider: React.FC<Props> = (props) => {
+  const [position, setPosition] = useState(0);
+  const [slided, setSlided] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const handleNextSlide = () => {
+    if (!slided) {
+      setSlided(true);
+    }
+    if (containerRef.current !== null) {
+      setPosition(position + 256);
+      console.log(containerRef.current.offsetWidth);
+      if (
+        position ===
+        -(wrapperRef.current!.offsetWidth - containerRef.current.offsetWidth)
+      ) {
+        setPosition(0);
+      }
+    }
+  };
+
+  const handlePrevSlide = () => {
+    if (!slided) {
+      setSlided(true);
+    }
+    if (containerRef.current !== null) {
+      setPosition(position - 256);
+      console.log(containerRef.current.offsetWidth);
+      if (
+        position ===
+        -(wrapperRef.current!.offsetWidth - containerRef.current.offsetWidth)
+      ) {
+        setPosition(0);
+      }
+    }
+  };
+
   return (
-    <div className={classes.slider}>
+    <div
+      className={classes.slider}
+      ref={containerRef}
+    >
       <div className={classes.sliderTitle}>{props.title}</div>
-      <div className={classes.container}>
+      <div
+        className={classes.container}
+        ref={wrapperRef}
+        style={{ transform: `translateX(${position}px)` }}
+      >
         {images.map((image, i) => (
-          <MovieItem key={i} src={image.src} alt={image.alt} />
+          <MovieItem
+            key={i}
+            src={image.src}
+            alt={image.alt}
+          />
         ))}
-        <div className={classes.arrow__right}></div>
+        <button
+          className={`${classes.arrow} ${classes.arrowRight}`}
+          onClick={handleNextSlide}
+          style={{ transform: `translateX(${-position}px)` }}
+        >
+          <span className={classes.arrow__circle}>
+            <img
+              className={classes.arrow__circleArrow}
+              src={ArrowRight}
+            />
+          </span>
+        </button>
+        {slided && (
+          <button
+          className={`${classes.arrow} ${classes.arrowLeft}`}
+          onClick={handlePrevSlide}
+          style={{ transform: `translateX(${-position}px)` }}
+        >
+          <span className={classes.arrow__circle} style={{left: '0'}}>
+            <img
+              className={classes.arrow__circleArrow}
+                src={ArrowRight}
+                style={{rotate: '180deg'}}
+            />
+          </span>
+        </button>
+          )}
+        
       </div>
     </div>
   );
